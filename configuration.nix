c8728098cfc9e8dc2d54ff025c7e394452362761
leo.habets <leo.habets@epita.fr>
2026-02-06 13:37:40 +0100

    feat(configuration): initrd swap partition

--- configuration.nix
+++ configuration.nix
@@ -9,15 +9,34 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.initrd.luks.devices."luks-d5656370-4e4e-4d1d-b004-13750e44dad2".device = "/dev/disk/by-uuid/d5656370-4e4e-4d1d-b004-13750e44dad2";
+
   networking.hostName = "leo-t480s";
   networking.networkmanager.enable = true;
 
   time.timeZone = "Europe/Paris";
 
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
   services.xserver.enable = true;
   services.xserver.autorun = true;
   services.xserver.windowManager.i3.enable = true;
   services.displayManager.sddm.enable = true;
+  services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "leo";
 
   users.users.leo = {
